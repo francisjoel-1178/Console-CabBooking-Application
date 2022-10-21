5a7fa2efb2e488a1e25bd8349d17f54c11898ad3
francisjoel-1178 <115210853+francisjoel-1178@users.noreply.github.com>
2022-10-21 20:21:13 +0530

    Main App

---
+++ demo/src/main/java/com/example/App.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import com.example.Display.FirstPage;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        FirstPage firstPage = new FirstPage();
+        firstPage.display();
+    }
+}
